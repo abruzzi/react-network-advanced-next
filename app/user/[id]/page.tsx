775d2fcdece2b7569b36981e04bad0977f396041
Juntao Qiu <juntao.qiu@gmail.com>
2024-01-07 16:27:12 +1100

    static generation on user (for the popoular users)

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -6,6 +6,10 @@ interface PageProps {
   };
 }
 
+export function generateStaticParams() {
+  return [{ id: "u1" }, { id: "u2" }, { id: "u3" }];
+}
+
 export default async function Page({ params }: PageProps) {
   return <Profile id={params.id} />;
 }
